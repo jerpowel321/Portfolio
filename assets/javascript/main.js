d2cf624c09db19e12b0674d5c603506047016ec6
Jennifer Powell <jerpowel321@gmail.com>
2019-03-18 20:46:37 -0700

    update portfolio to be on 1 htmlfile

---
+++ assets/javascript/main.js
@@ -0,0 +1,35 @@
+(function () {
+
+    var bv = new Bideo();
+    bv.init({
+      // Video element
+      videoEl: document.querySelector('#background_video'),
+  
+      // Container element
+      container: document.querySelector('#container'),
+  
+      // Resize
+      resize: true,
+  
+      // autoplay: false,
+  
+      isMobile: window.matchMedia('(max-width: 768px)').matches,
+  
+      // playButton: document.querySelector('#play'),
+      // pauseButton: document.querySelector('#pause'),
+  
+      // Array of objects containing the src and type
+      // of different video formats to add
+      src: [
+        {
+          src: 'assets/javascript/ocean.jpg',
+          type: 'video/mp4'
+        },
+        {
+          src: 'assets/javascript/ocean.mp4',
+          type: 'video/webm;codecs="vp8, vorbis"'
+        }
+      ],
+ 
+    });
+  }());
